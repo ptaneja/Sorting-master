8ce8abff7083294aa66c37bed702cdb8acc82f66
Pankaj Taneja <ptaneja@visa.com>
2016-02-09 13:16:42 -0800

    test

---
+++ Sorting/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Sorting
+//
+//  Created by Tringapps Inc on 6/29/14.
+//  Copyright (c) 2014 TringApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
